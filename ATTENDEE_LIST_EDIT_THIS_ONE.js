35836c4254a5924fbe60d21d88f52b51d519d7b6
Michael Easter <codetojoy@gmail.com>
2015-05-12 06:14:00 -0300

    testing static pages / web hosting

---
+++ ATTENDEE_LIST_EDIT_THIS_ONE.js
@@ -0,0 +1,15 @@
+// auto-generated from NAMES.txt
+var ATTENDEE_LIST = [
+"Shayne Walker",
+"Kevin MacPhail",
+"Christian Southgate",
+"Dmitrii Shesterkin",
+"Ben",
+"Callista Tan",
+"Mark Wright",
+"Evan Porter",
+"Chris Mears",
+"Krista G-M",
+"Quinn Jackson",
+"Nolan Phillips",
+];
---
+++ utils.js
@@ -0,0 +1,41 @@
+/**
+ * from http://stackoverflow.com/a/12646864/12704
+ * converted to AMD spec
+ */
+define(function () {
+    return {
+        shuffleNames: function (array) {
+
+            for (var i = array.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+
+            return array;
+        },
+
+        pickOne: function (array) {
+            var index = this.getRandom(0, array.length - 1);
+            return array[index];
+        },
+
+        oneInNChance: function (numChances) {
+            var result = false;
+
+            var x = this.getRandom(1, numChances);
+
+            if (x == 1) {
+                result = true;
+            }
+
+            return result;
+        },
+
+        getRandom: function (min, max) {
+            var x = Math.floor(Math.random() * (max - min + 1)) + min;
+            return x;
+        }
+    };
+});
