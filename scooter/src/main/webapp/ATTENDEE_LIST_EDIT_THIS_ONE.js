0d5dab72d21bc8bbb10e0f2e158702a3aa2a81e5
Michael Easter <codetojoy@gmail.com>
2015-05-13 19:20:15 -0300

    latest attendee list

--- scooter/src/main/webapp/ATTENDEE_LIST_EDIT_THIS_ONE.js
+++ scooter/src/main/webapp/ATTENDEE_LIST_EDIT_THIS_ONE.js
@@ -1,7 +1,6 @@
 // auto-generated from NAMES.txt
 var ATTENDEE_LIST = [
 "Shayne Walker",
-"Kevin MacPhail",
 "Christian Southgate",
 "Dmitrii Shesterkin",
 "Ben",
@@ -12,4 +11,6 @@ var ATTENDEE_LIST = [
 "Krista G-M",
 "Quinn Jackson",
 "Nolan Phillips",
+"Ron MacKay",
+"Ross Evans",
 ];
