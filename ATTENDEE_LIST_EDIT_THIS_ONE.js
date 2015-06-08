bd8e4c38a234fd5678930c16f547544564ffc010
Michael Easter <codetojoy@gmail.com>
2015-06-08 19:43:25 -0300

    modified for June

--- ATTENDEE_LIST_EDIT_THIS_ONE.js
+++ ATTENDEE_LIST_EDIT_THIS_ONE.js
@@ -1,18 +1,12 @@
 // auto-generated from NAMES.txt
 var ATTENDEE_LIST = [
-"Shayne Walker",
-"Christian Southgate",
-"Dmitrii Shesterkin",
-"Ben",
-"Callista Tan",
-"Mark Wright",
-"Evan Porter",
-"Chris Mears",
-"Krista G-M",
-"Quinn Jackson",
+"Jason Wentzel",
+"Joab Souza",
 "Nolan Phillips",
 "Ron MacKay",
-"Ross Evans",
-"Ross Evans +1",
-"Mike Berger"
+"Christian Southgate",
+"Ben",
+"Callista Tan",
+"Kevin MacPhail",
+"Evan Porter",
 ];
