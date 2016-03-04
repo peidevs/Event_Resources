0f2bc968690521c34c2986a78fbaa0a4a3d5648c
Michael Easter <codetojoy@gmail.com>
2016-03-04 19:08:31 -0400

    PEI Open Data Hackathon

--- ATTENDEE_LIST_EDIT_THIS_ONE.js
+++ ATTENDEE_LIST_EDIT_THIS_ONE.js
@@ -1,12 +1,27 @@
 // auto-generated from NAMES.txt
 var ATTENDEE_LIST = [
-"Callista Tan",
-"Mike Berger",
-"Nitheen Rao T",
-"Mark Wright",
+"Ifo Ikede",
+"Nikita Volodin",
+"Jeremy Thompson",
+"Tyler Veinot",
+"Celito Felipetto",
+"Chris Stewart",
+"Christian Southgate",
+"Tom Tang",
+"Nathan Murnaghan",
+"T Wood",
+"Julian Keller",
+"Sarah Thompson",
+"Tristan Vuong",
+"Sishir Mohan",
+"Nitheen Rao",
+"Kelvin Liang",
+"Ben Sinnamon",
+"josie roman",
+"Patrick Williams",
+"mark Wright"
+"philip parke",
 "Evan Porter",
-"Onoriode Ikede",
-"Jennifer Gavin",
-"Robert Hughes",
-"Alesia"
+"Michael Berger",
+"Adam LeBlanc"
 ];
