9e5a60025e86d484d19a8d272dfcd52f25f10d65
Michael Easter <codetojoy@gmail.com>
2016-09-07 15:12:29 -0300

    PEI Devs SEP 2016

--- ATTENDEE_LIST_EDIT_THIS_ONE.js
+++ ATTENDEE_LIST_EDIT_THIS_ONE.js
@@ -1,21 +1,15 @@
 // auto-generated from NAMES.txt
 var ATTENDEE_LIST = [
-"Steve B",
-"Steve + 1",
-"Ross E",
-"Ross + 1",
-"Colin C",
-"James O",
+"Sean M",
 "Michael G",
+"Nolan P",
+"Mike B",
 "Nimna E",
 "Alexander H",
+"Mike M",
+"Jim M",
+"Marie-Eve",
+"Marie-Eve + 1",
 "Timothy S",
-"Timothy + 1",
-"Brandon B",
-"roy k",
-"Jahanzaib R",
-"Jingtao L",
-"Mike B",
-"Cristian T",
-"Mike M"
+"Timothy + 1"
 ];
